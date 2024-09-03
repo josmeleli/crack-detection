4a14231dc691d9f655352f7d4d36c8b168fc1d6d
josmel <josmelvt@gmail.com>
2024-09-02 21:48:07 -0500

    api apirailway

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 import tempfile
-from tensorflow.keras.models import load_model
-from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import JSONResponse
 import numpy as np
 import cv2 as cv
-from skimage import morphology
-from scipy.ndimage import distance_transform_edt
 import matplotlib.pyplot as plt
 from PIL import Image
 import io
-import tensorflow as tf
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import base64
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from tensorflow.keras.layers import DepthwiseConv2D as BaseDepthwiseConv2D
+from skimage import morphology
+from scipy.ndimage import distance_transform_edt
 from io import BytesIO
+import base64
 
+# Define una clase personalizada para DepthwiseConv2D
+class CustomDepthwiseConv2D(BaseDepthwiseConv2D):
+    def __init__(self, **kwargs):
+        # Elimina el argumento 'groups' ya que no es compatible
+        kwargs.pop('groups', None)
+        super().__init__(**kwargs)
 
-# Inicializa la aplicación FastAPI
-app = FastAPI()
-
-# Cargar el modelo con la capa personalizada
+# Define una capa personalizada si es necesario
 class FixedDropout(tf.keras.layers.Dropout):
     def __init__(self, rate, noise_shape=None, seed=None, **kwargs):
         super(FixedDropout, self).__init__(rate, noise_shape, seed, **kwargs)
@@ -27,53 +31,51 @@ class FixedDropout(tf.keras.layers.Dropout):
         config = super(FixedDropout, self).get_config()
         return config
 
-model_path = 'models\model_effnetB1_512pix_fold3_full_84283.h5'
-model = load_model(model_path, custom_objects={'FixedDropout': FixedDropout}, compile=False)
+# Inicializa la aplicación FastAPI
+app = FastAPI()
+
+# Ruta al archivo del modelo
+model_path = 'models/model_effnetB1_512pix_fold3_full_84283.h5'
+
+# Cargar el modelo con la capa personalizada
+try:
+    model = load_model(model_path, custom_objects={'FixedDropout': FixedDropout, 'DepthwiseConv2D': CustomDepthwiseConv2D}, compile=False)
+    print("Modelo de segmentación cargado con éxito")
+except Exception as e:
+    print(f"Error al cargar el modelo de segmentación: {e}")
 
 # Cargar el modelo de clasificación
-classification_model_path = 'models\CNN_surface_crack_detection.h5'
-classification_model = tf.keras.models.load_model(classification_model_path)
+classification_model_path = 'models/CNN_surface_crack_detection.h5'
+try:
+    classification_model = tf.keras.models.load_model(classification_model_path)
+    print("Modelo de clasificación cargado con éxito")
+except Exception as e:
+    print(f"Error al cargar el modelo de clasificación: {e}")
 
 # Función para medir el ancho de las grietas
 def crack_width_measure(binary_image, display_results=True):
-    """
-    Measure the crack widths along its length and identify the maximum crack width in a binary image.
-    
-    :param binary_image: A 2D NumPy array representing the binary image (where the crack is represented as by white pixels).
-    :param display_results: If True, display the results.
-    :return: A tuple containing an array of crack widths and the maximum crack width.
-    """
-    # Ensuring the image being used is a binary image
     binary_image = binary_image > 0
     
-    # Applying skeletonisation
+    # Aplicando skeletonisation
     skeleton = morphology.skeletonize(binary_image)
     
-    # Applying distance transform
+    # Aplicando distance transform
     distance_transform = distance_transform_edt(binary_image)
     
-    # Measuring crack widths along the skeleton
-    crack_widths = distance_transform[skeleton]*2
+    # Midiendo los anchos de grietas a lo largo del esqueleto
+    crack_widths = distance_transform[skeleton] * 2
     
-    # Identifying maximum crack width in the image
+    # Identificando el ancho máximo de la grieta en la imagen
     max_crack_width = np.max(crack_widths) if crack_widths.size > 0 else 0
     
     if display_results:
-        # Display the crack widths along the skeleton
+        # Mostrar los anchos de grietas a lo largo del esqueleto
         plt.figure(figsize=(6, 6))
         plt.imshow(binary_image, cmap='gray')
         plt.scatter(*np.where(skeleton)[::-1], c=crack_widths, cmap='jet', s=10, label='Crack Widths')
         plt.colorbar(label='Ancho Máximo en (pixels)')
         plt.title('Grieta con Ancho Máximo')
         plt.axis('off')
-        
-        # Print value of max crack width
-        print(f"Ancho máximo de grieta: {max_crack_width} pixels")
-        
-        text_x = 10  # 10 pixels from the left
-        text_y = 20  # 20 pixels from the top
-        
-        plt.text(text_x, text_y, f"Max Width: {max_crack_width:.2f} pixels", color='white')
         
         # Guardar la imagen en memoria en lugar de en el disco
         buf = BytesIO()
@@ -94,20 +96,16 @@ def process_and_measure_crack(binary_image_array):
     crack_widths, max_crack_width, image_base64 = crack_width_measure(binary_image_array)
     return crack_widths, max_crack_width, image_base64
 
-
 def load_and_preprocess_image(img_path):
-    img = load_img(img_path, target_size=(120, 120))
-    img_array = img_to_array(img)
+    img = Image.open(img_path).resize((120, 120))  # Usar PIL para cargar y redimensionar
+    img_array = np.array(img, dtype=np.float32)  # Asegúrate de que la imagen esté en float32
+    img_array /= 255.0  # Normalizar la imagen
     img_array = np.expand_dims(img_array, axis=0)
-    img_array /= 255.0
     return img_array
-
-
 
 # Endpoint POST para procesar la imagen
 @app.post("/predict/")
 async def predict(file: UploadFile = File(...)):
-    # Leer la imagen
     contents = await file.read()
 
     # Crear un archivo temporal para usar con load_img
@@ -115,11 +113,11 @@ async def predict(file: UploadFile = File(...)):
         temp_image.write(contents)
         temp_image_path = temp_image.name
 
-    # Usar load_img para cargar la imagen desde el archivo temporal
-    image = load_img(temp_image_path, target_size=(416, 416))  # Ajusta el tamaño si es necesario
+    # Usar PIL para cargar la imagen desde el archivo temporal
+    image = Image.open(temp_image_path).resize((416, 416))
 
     # Preprocesar la imagen para el modelo
-    img_array = img_to_array(image)
+    img_array = np.array(image)
     img_array = np.expand_dims(img_array, axis=0)
     img_array = img_array / 255.0  # Normalizar la imagen
 
@@ -136,46 +134,35 @@ async def predict(file: UploadFile = File(...)):
     # Devolver los resultados
     return JSONResponse(content={"max_crack_width": max_crack_width, "max_width_image": highlighted_image_base64})
 
-# Function to detect circles
+# Función para detectar y medir círculos
 def detect_and_measure_circle(image: np.ndarray):
-    # Convirtiendo de espacio de color BGR a HSV
     hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
 
-    # Definiendo límites inferior y superior para el color rojo
     lower_red1 = np.array([0, 70, 50])
     upper_red1 = np.array([10, 255, 255])
     lower_red2 = np.array([170, 70, 50])
     upper_red2 = np.array([180, 255, 255])
 
-    # Creando máscaras para ambos rangos de rojo y luego combinándolas
     mask1 = cv.inRange(hsv, lower_red1, upper_red1)
     mask2 = cv.inRange(hsv, lower_red2, upper_red2)
     mask = cv.bitwise_or(mask1, mask2)
 
-    # Operación morfológica para reducir el ruido en la máscara
     kernel = np.ones((3, 3), np.uint8)
     mask = cv.morphologyEx(mask, cv.MORPH_OPEN, kernel, iterations=2)
 
-    # Filtrando el color rojo de la imagen original usando la máscara
     res = cv.bitwise_and(image, image, mask=mask)
 
-    # Conversión a escala de grises para mejorar la detección de círculos
     gray = cv.cvtColor(res, cv.COLOR_BGR2GRAY)
-
-    # Aplicando desenfoque gaussiano para reducir el ruido y mejorar la detección de círculos
     gray_blurred = cv.GaussianBlur(gray, (9, 9), 2, 2)
 
-    # Aplicando detección de círculos de Hough
-    circles = cv.HoughCircles(gray_blurred, cv.HOUGH_GRADIENT, 1, gray_blurred.shape[0]/8, param1=100, param2=30, minRadius=0, maxRadius=0)
+    circles = cv.HoughCircles(gray_blurred, cv.HOUGH_GRADIENT, 1, gray_blurred.shape[0] / 8, param1=100, param2=30, minRadius=0, maxRadius=0)
 
-    # Dibujando el círculo más grande y mostrando el diámetro si se detecta alguno
     if circles is not None:
         circles = np.uint16(np.around(circles))
         largest_circle = max(circles[0, :], key=lambda x: x[2])
         cv.circle(image, (largest_circle[0], largest_circle[1]), largest_circle[2], (0, 255, 0), 5)
         cv.circle(image, (largest_circle[0], largest_circle[1]), 2, (0, 0, 255), 5)
         
-        # Calculando el diámetro
         diameter = largest_circle[2] * 2
         
         return diameter
@@ -193,8 +180,6 @@ async def detect_circles_endpoint(file: UploadFile = File(...)):
         return JSONResponse(content={"message": f"El diámetro del círculo es {diameter} píxeles"})
     else:
         return JSONResponse(content={"message": "No se detectaron círculos."})
-    
-
 
 @app.post("/classify/")
 async def classify(file: UploadFile = File(...)):
@@ -214,7 +199,6 @@ async def classify(file: UploadFile = File(...)):
         return JSONResponse(content={"prediction": "Crack Detected"})
     else:
         return JSONResponse(content={"prediction": "No Crack Detected"})
-
 
 # Run the application
 if __name__ == "__main__":
